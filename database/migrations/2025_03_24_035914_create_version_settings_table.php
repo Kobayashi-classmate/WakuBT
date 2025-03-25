5de7d7bad4981aa58159b775238f18fbe7f177fb
Your Name <you@example.com>
2025-03-25 01:57:09 +0000

    modified:   database/migrations/2025_03_24_035914_create_version_settings_table.php

--- database/migrations/2025_03_24_035914_create_version_settings_table.php
+++ database/migrations/2025_03_24_035914_create_version_settings_table.php
@@ -4,8 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+use App\Models\VersionSettings;
+
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,18 +14,68 @@ return new class extends Migration
     {
         Schema::create('version_settings', function (Blueprint $table) {
             $table->id();
-            $table->string('software_name')->comment('软件名称');
-            $table->string('version_number')->comment('版本号');
-            $table->timestamp('release_date')->comment('发布时间');
-            $table->text('change_log')->comment('更新日志');
-            $table->boolean('is_enabled')->default(true)->comment('是否启用');
-            $table->boolean('is_visible')->default(false)->comment('是否展示');
+            $table->string('software_name');
+            $table->string('version_number');
+            $table->timestamp('release_date');
+            $table->text('change_log');
+            $table->boolean('is_enabled')->default(true);
+            $table->boolean('is_visible')->default(false);
             $table->timestamps();
 
             // 添加索引
             $table->index('version_number');
             $table->index('software_name');
         });
+
+
+        /**
+         * 初始化数据库内容
+         */
+
+        VersionSettings::create([
+            'software_name' => '1',
+            'version_number' => '9.5.0',
+            'release_date' => '2025-03-24 00:00:00',
+            'change_log' => '暂无更新日志',
+            'is_enabled' => 1,
+            'is_visible' => 1,
+        ]);
+
+        VersionSettings::create([
+            'software_name' => '2',
+            'version_number' => '8.2.1',
+            'release_date' => '2024-12-06 00:00:00',
+            'change_log' => '暂无更新日志',
+            'is_enabled' => '1',
+            'is_visible' => '1',
+        ]);
+
+        VersionSettings::create([
+            'software_name' => '3',
+            'version_number' => '7.0.13',
+            'release_date' => '2024-11-17 00:00:00',
+            'change_log' => '暂无更新日志',
+            'is_enabled' => '1',
+            'is_visible' => '1',
+        ]);
+
+        VersionSettings::create([
+            'software_name' => '4',
+            'version_number' => '2.3.0',
+            'release_date' => '2024-04-24 00:00:00',
+            'change_log' => '暂无更新日志',
+            'is_enabled' => '1',
+            'is_visible' => '1',
+        ]);
+
+        VersionSettings::create([
+            'software_name' => '5',
+            'version_number' => '5.3',
+            'release_date' => '2025-02-24 15:19:17',
+            'change_log' => '暂无更新日志',
+            'is_enabled' => '1',
+            'is_visible' => '1',
+        ]);
     }
 
     /**
